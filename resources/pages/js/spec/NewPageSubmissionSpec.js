172aded3f1dcd5fe55ed513e469d8d5a33bae356
PMurph <murph.pm@gmail.com>
2015-12-27 11:15:05 +1100

    Added function to convert form data to json, and fixed wiki page new page template to use the appropriate script

--- resources/pages/js/spec/NewPageSubmissionSpec.js
+++ resources/pages/js/spec/NewPageSubmissionSpec.js
@@ -1,5 +1,42 @@
 describe("New Wiki Page Form Submission", function() {
-    it("should be a placeholder", function() {
-        expect(true).toBeTruthy();
+    describe('formToJson', function() {
+        var firstNamedElement;
+        var secondNamedElement;
+        var nonNamedElement;
+        
+        beforeEach(function() {
+            firstNamedElement = {'getAttribute': function() {}};
+            spyOn(firstNamedElement, 'getAttribute').and.returnValue('first');
+            firstNamedElement.value = '1st'
+            
+            secondNamedElement = {'getAttribute': function() {}};
+            spyOn(secondNamedElement, 'getAttribute').and.returnValue('second');
+            secondNamedElement.value = '2nd';
+            
+            nonNamedElement = {'getAttribute': function() {}}
+            spyOn(nonNamedElement, 'getAttribute').and.returnValue(null);
+        });
+        
+        afterEach(function() {
+            expect(firstNamedElement.getAttribute).toHaveBeenCalledWith('name');
+            expect(secondNamedElement.getAttribute).toHaveBeenCalledWith('name');
+        });
+        
+        it('should create a json object with form element names to values', function() {
+            var form = [firstNamedElement, secondNamedElement];
+            
+            formJson = formToJson(form);
+            
+            expect(formJson).toEqual('{"first":"1st","second":"2nd"}');
+        });
+        
+        it('should not contain the value of a form element with no name', function() {
+            var form = [firstNamedElement, secondNamedElement, nonNamedElement];
+            
+            formJson = formToJson(form);
+            
+            expect(formJson).toEqual('{"first":"1st","second":"2nd"}');
+            expect(nonNamedElement.getAttribute).toHaveBeenCalledWith('name');
+        });
     });
 });
--- resources/pages/js/src/NewPageSubmission.js
+++ resources/pages/js/src/NewPageSubmission.js
@@ -1,2 +1,19 @@
-function createNewPage(form) {
-};
+function createNewPage(formJson, xml_http_request) {
+    
+}
+
+function formToJson(form) {
+    var formValues = {}
+    
+    for(var element in form) {
+        var elementName = form[element].getAttribute('name');
+        if(elementName !== null) {
+            formValues[elementName] = form[element].value;
+        }
+    }
+    
+    return JSON.stringify(formValues);
+}
+
+function handleCreatePageResponse(xml_http_request) {
+}
