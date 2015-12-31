e1c0346324371ea01e6b6ba0833e3c136826479d
PMurph <murph.pm@gmail.com>
2015-12-31 14:20:41 +1100

    Added scenario for editting and wiki page and added steps(some placeholder steps) for the scenarios

--- pages/features/wikipage_edit.feature
+++ pages/features/wikipage_edit.feature
@@ -5,7 +5,19 @@ Feature: As a software developer,
 Scenario: Viewing the edit page
     Given that the TestProject page exists
     And given that I view the TestProject page
-    It should have a link to the link to the edit page
-    Given that I view the TestProject edit page
+    It should have a link to the edit operation page
+    Given that I view the TestProject edit operation page
     It should have a place to enter the title of the page
     And it should have a place to enter the page_content of the page
+    
+ Scenario: Editing a wiki page
+    Given that the EditTestPage page exists
+    And given that I view the EditTestPage edit operation page
+    And given that I enter "EdittedTestPage" as the title
+    And given that I enter "This is a test to see if the editting feature is working" as the page_content
+    And given that I save the edits
+    I should receive a message stating that the EditTestPage page has been saved
+    Given that I view the EditTestPage detail operation page
+    It should have "EdittedTestPage" as the title
+    And it should have "EdittedTestPage" as the h1 heading
+    And it should have "This is a test to see if the editting feature is working" as the content
---
+++ pages/features/wikipage_edit.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import django.core.urlresolvers
+import django.test.client
+import django.test.runner
+import django.utils
+import json
+import pages.models
+import lettuce
+
+@lettuce.before.all
+def setUp():
+    # Hack. See wikipage_view.py for more details
+    lettuce.world.test_runner = django.test.runner.DiscoverRunner()
+    lettuce.world.old_config = lettuce.world.test_runner.setup_databases()
+    lettuce.world.test_client = django.test.client.Client()
+    
+@lettuce.after.all
+def tearDown(*args):
+    lettuce.world.test_runner.teardown_databases(lettuce.world.old_config)
+    
+@lettuce.step(u'[And ]{0,1}[G|g]iven that I view the ([^\s]+) ([^\s]+) operation page')
+def given_i_view_pages_operation(step, page_title, page_operation):
+    page_operation = 'pages:%s' % (page_operation)
+    lettuce.world.last_response = lettuce.world.test_client.get(django.core.urlresolvers.reverse(page_operation, args=(page_title,)))
+    
+@lettuce.step(u'[And ]{0,1}[G|g]iven that I save the edits')
+def given_that_i_save_edits(step):
+    assert(False)
+    
+@lettuce.step(u'[And ]{0,1}I should receive a message stating that the ([^\s]+) page has been saved')
+def should_receive_message_that_page_was_saved(step, page_url):
+    assert(False)
+    
+@lettuce.step(u'[And ]{0,1}[I|i]t should have a link to the ([^\s]+) operation page')
+def should_have_link_to_page_operation(step, page_operation):
+    page_link = "href='%s/'" % (page_operation)
+    assert(page_link in lettuce.world.last_response.content)
